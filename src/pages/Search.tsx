46dfd41e2eecbcd49d49b638c4ce45499b4afc58
Sepide Norouzi <sepidenorouzi17@gmail.com>
2026-08-27 17:46:12 +0330

    fix search header

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -7,7 +7,7 @@ export default function Search() {
   const [query, setQuery] = useState("");
 
   return (
-    <main className="flex flex-col gap-8 p-8">
+    <main className="flex flex-col gap-8 p-8 pt-20 md:pt-0">
       <div>
         <h1 className="font-heading text-3xl font-bold text-[var(--text)]">
           Search
