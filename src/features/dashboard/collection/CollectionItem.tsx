4ec398c82174e89c16daf628027365969328fc3b
Sepide Norouzi <sepidenorouzi17@gmail.com>
2026-09-04 18:32:04 +0330

    change text size

--- src/features/dashboard/collection/CollectionItem.tsx
+++ src/features/dashboard/collection/CollectionItem.tsx
@@ -72,7 +72,7 @@ export default function CollectionItem({
             line-clamp-2
 
             font-heading
-            text-[12px]
+            text-[11px]
             font-semibold
             leading-[1.25]
 
