93aaaf0df51031cb7e23e289f9c01e902c249d82
Arashebik <koroushebi@gmail.com>
2026-09-24 06:50:54 -0700

    changed avatar to string format

--- user_module/models.py
+++ user_module/models.py
@@ -48,11 +48,7 @@ class User(AbstractUser):
 
     email = None
 
-    avatar = models.ImageField(
-        upload_to="avatars/",
-        null=True,
-        blank=True,
-    )
+    avatar = models.CharField(max_length=255, blank=True, null=True)
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
